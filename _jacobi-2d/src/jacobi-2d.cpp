9f1d1dbdcbcb816fc5e33e1487b0f900c2595ab2
Luca Omini <luca.omini@usi.ch>
2022-02-06 21:16:51 +0100

    jacobi opt version cache

--- _jacobi-2d/src/jacobi-2d.cpp
+++ _jacobi-2d/src/jacobi-2d.cpp
@@ -48,6 +48,60 @@ void init_array (int n, DATA_TYPE **A, DATA_TYPE **B)
 }
 
 
+//void kernel_jacobi_2d_vector(int tsteps,int n, DATA_TYPE **A,DATA_TYPE **B)
+//{
+//    _MMR_f64    xU;
+//    _MMR_f64    xUtmp;
+//    _MMR_f64    xUleft;
+//    _MMR_f64    xUright;
+//    _MMR_f64    xUtop;
+//    _MMR_f64    xUbottom;
+//    _MMR_f64    xConstant;
+//
+//    double diff, sum=0.0;
+//    unsigned long int izq,der;
+//    int size_y = n-2;
+//    int size_x = n-2;
+//
+////    unsigned long int gvl = __builtin_epi_vsetvl(size_y, __epi_e64, __epi_m1);
+//    int limit = loop_bound(SPECIES_512, size_y);
+//
+//    xConstant = _MM_SET_f64(0.2);
+//
+//    for (int j=1; j< limit; j=j+SPECIES_512)
+//    {
+////        gvl = __builtin_epi_vsetvl(size_y-j+1, __epi_e64, __epi_m1);
+//
+//        xU = _MM_LOAD_f64(&A[1][j]);
+//        xUtop = _MM_LOAD_f64(&A[0][j]);
+//        xUbottom = _MM_LOAD_f64(&A[2][j]);
+//
+//        for (int i=1; i<=size_y; i++)
+//        {
+//            if(i!=1)
+//            {
+//                xUtop = xU;
+//                xU =  xUbottom;
+//                xUbottom =  _MM_LOAD_f64(&A[i+1][j]);
+//            }
+////            izq = *(unsigned long int*)&A[i][j-1];
+////            der = *(unsigned long int*)&A[i][j+gvl];
+////            xUleft = _MM_VSLIDE1UP_f64(xU,izq,gvl); // take the next SPECIES el
+////            xUright = _MM_VSLIDE1DOWN_f64(xU,der,gvl);
+//            xUleft = _MM_LOAD_f64(&A[i][j-1]);
+//            xUright = _MM_LOAD_f64(&A[i][j+1]);
+//            xUtmp = _MM_ADD_f64(xUleft,xUright);
+//            xUtmp = _MM_ADD_f64(xUtmp,xUtop);
+//            xUtmp = _MM_ADD_f64(xUtmp,xUbottom);
+//            xUtmp = _MM_ADD_f64(xUtmp,xU);
+//            xUtmp = _MM_MUL_f64(xUtmp,xConstant);
+//            _MM_STORE_f64(&B[i][j], xUtmp);
+//        }
+//    }
+////    FENCE();
+//}
+
+
 void kernel_jacobi_2d_vector(int tsteps,int n, DATA_TYPE **A,DATA_TYPE **B)
 {
     _MMR_f64    xU;
@@ -58,8 +112,9 @@ void kernel_jacobi_2d_vector(int tsteps,int n, DATA_TYPE **A,DATA_TYPE **B)
     _MMR_f64    xUbottom;
     _MMR_f64    xConstant;
 
-    double diff, sum=0.0;
-    unsigned long int izq,der;
+    int i, j;
+//    double diff, sum=0.0;
+//    unsigned long int izq,der;
     int size_y = n-2;
     int size_x = n-2;
 
@@ -68,26 +123,11 @@ void kernel_jacobi_2d_vector(int tsteps,int n, DATA_TYPE **A,DATA_TYPE **B)
 
     xConstant = _MM_SET_f64(0.2);
 
-    for (int j=1; j< limit; j=j+SPECIES_512)
-    {
-//        gvl = __builtin_epi_vsetvl(size_y-j+1, __epi_e64, __epi_m1);
-
-        xU = _MM_LOAD_f64(&A[1][j]);
-        xUtop = _MM_LOAD_f64(&A[0][j]);
-        xUbottom = _MM_LOAD_f64(&A[2][j]);
-
-        for (int i=1; i<=size_y; i++) 
-        {
-            if(i!=1)
-            {
-                xUtop = xU;
-                xU =  xUbottom;
-                xUbottom =  _MM_LOAD_f64(&A[i+1][j]);
-            }
-//            izq = *(unsigned long int*)&A[i][j-1];
-//            der = *(unsigned long int*)&A[i][j+gvl];
-//            xUleft = _MM_VSLIDE1UP_f64(xU,izq,gvl); // take the next SPECIES el
-//            xUright = _MM_VSLIDE1DOWN_f64(xU,der,gvl);
+    for (i = 1; i <= size_y; i++) {
+        for (j = 1; j < limit; j += SPECIES_512) {
+            xU = _MM_LOAD_f64(&A[i][j]);
+            xUtop = _MM_LOAD_f64(&A[i-1][j]);
+            xUbottom = _MM_LOAD_f64(&A[i+1][j]);
             xUleft = _MM_LOAD_f64(&A[i][j-1]);
             xUright = _MM_LOAD_f64(&A[i][j+1]);
             xUtmp = _MM_ADD_f64(xUleft,xUright);
@@ -97,6 +137,42 @@ void kernel_jacobi_2d_vector(int tsteps,int n, DATA_TYPE **A,DATA_TYPE **B)
             xUtmp = _MM_MUL_f64(xUtmp,xConstant);
             _MM_STORE_f64(&B[i][j], xUtmp);
         }
+    }
+
+//    for (int j=1; j< limit; j=j+SPECIES_512)
+//    {
+////        gvl = __builtin_epi_vsetvl(size_y-j+1, __epi_e64, __epi_m1);
+//
+//        xU = _MM_LOAD_f64(&A[1][j]);
+//        xUtop = _MM_LOAD_f64(&A[0][j]);
+//        xUbottom = _MM_LOAD_f64(&A[2][j]);
+//
+//        for (int i=1; i<=size_y; i++)
+//        {
+//            if(i!=1)
+//            {
+//                xUtop = xU;
+//                xU =  xUbottom;
+//                xUbottom =  _MM_LOAD_f64(&A[i+1][j]);
+//            }
+////            izq = *(unsigned long int*)&A[i][j-1];
+////            der = *(unsigned long int*)&A[i][j+gvl];
+////            xUleft = _MM_VSLIDE1UP_f64(xU,izq,gvl); // take the next SPECIES el
+////            xUright = _MM_VSLIDE1DOWN_f64(xU,der,gvl);
+//            xUleft = _MM_LOAD_f64(&A[i][j-1]);
+//            xUright = _MM_LOAD_f64(&A[i][j+1]);
+//            xUtmp = _MM_ADD_f64(xUleft,xUright);
+//            xUtmp = _MM_ADD_f64(xUtmp,xUtop);
+//            xUtmp = _MM_ADD_f64(xUtmp,xUbottom);
+//            xUtmp = _MM_ADD_f64(xUtmp,xU);
+//            xUtmp = _MM_MUL_f64(xUtmp,xConstant);
+//            _MM_STORE_f64(&B[i][j], xUtmp);
+//        }
+//    }
+
+    for (i = 1; i < n - 1; i++) {
+        for (j = limit + 1; j < n - 1; j++)
+            B[i][j] = (0.2) * (A[i][j] + A[i][j - 1] + A[i][1 + j] + A[1 + i][j] + A[i - 1][j]);
     }
 //    FENCE();
 }
@@ -123,7 +199,7 @@ static void kernel_jacobi_2d_intrinsic(int tsteps,int n, DATA_TYPE **A,DATA_TYPE
 
     for (t = 0; t < tsteps; t++)
     {
-        kernel_jacobi_2d_vector(tsteps,n, A,B);
+        kernel_jacobi_2d_vector(tsteps,n, A, B);
         kernel_jacobi_2d_vector(tsteps,n, B,A);
     }
 }
@@ -192,7 +268,7 @@ int main(int argc, char** argv)
   long long end = get_time();
   printf("time: %lf\n", elapsed_time(start, end));
 #ifdef RESULT_PRINT
-  output_printfile(n,A, outfilename );
+//  output_printfile(n,A, outfilename );
 #endif  // RESULT_PRINT
 
     /* Initialize array(s). */
@@ -208,7 +284,7 @@ int main(int argc, char** argv)
     end = get_time();
     printf("time: %lf\n", elapsed_time(start, end));
 #ifdef RESULT_PRINT
-    output_printfile(n,A, outfilename );
+//    output_printfile(n,A, outfilename );
 #endif  // RESULT_PRINT
 
   /* Be clean. */
